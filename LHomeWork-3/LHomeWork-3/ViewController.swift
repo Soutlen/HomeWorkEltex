f13836595599ab66d84bd6a41bb2e0a58d69bfd3
Eugene <tt5tt5291@gmail.com>
2026-03-18 21:11:32 +0700

    Выполнил ДЗ 3

---
+++ LHomeWork-3/LHomeWork-3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LHomeWork-3
+//
+//  Created by Евгений Глоба on 3/18/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
