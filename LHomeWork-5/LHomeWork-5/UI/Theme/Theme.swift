6a6d20896ba23b5b66f9a6a45fe07b848432a7da
Eugene <tt5tt5291@gmail.com>
2026-03-23 21:55:20 +0700

    Выполнил ДЗ 5 + гит игнор добваил

---
+++ LHomeWork-5/LHomeWork-5/UI/Theme/Theme.swift
@@ -0,0 +1,15 @@
+//
+//  Theme.swift
+//  LHomeWork-5
+//
+//  Created by Евгений Глоба on 3/23/26.
+//
+
+import UIKit
+
+enum Theme {
+    static let font = ThemeFont.self
+    static let size = ThemeSize.self
+    static let spacing = ThemeSpacing.self
+    static let color = ThemePalette.self
+}
---
+++ LHomeWork-5/LHomeWork-5/UI/Theme/ThemeFont.swift
@@ -0,0 +1,22 @@
+//
+//  ThemeFont.swift
+//  LHomeWork-5
+//
+//  Created by Евгений Глоба on 3/23/26.
+//
+
+import UIKit
+
+enum ThemeFont {
+    static func regular(size: CGFloat) -> UIFont {
+        UIFont.systemFont(ofSize: size)
+    }
+    
+    static func medium(size: CGFloat) -> UIFont {
+        UIFont.systemFont(ofSize: size)
+    }
+    
+    static func bold(size: CGFloat) -> UIFont {
+        UIFont.systemFont(ofSize: size)
+    }
+}
